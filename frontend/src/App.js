182d9e4fe1eecf2b2728d69c93f4e8bc67346af0
gmshuvo <gm.shuvo.ru.cse@gmail.com>
2021-05-01 11:25:22 +0600

    product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
 import "./App.css";
 import "./bootstrap.min.css";
 import Header from "./Components/Header/Header";
@@ -9,15 +9,21 @@ import ShopScreen from "./screens/ShopScreen";
 function App() {
   return (
     <div className="App">
-      <p>dihan abir</p>
-      <Header/>
-      <main className="container">
+     
+      {/* <Header/> */}
+      <Router>
+
+        <Route path='/collections/products' exact component={ShopScreen}/>
+      </Router>
+
+      {/* <main className="container">
         <Router>
-          <Route to="/" exact component={HomeScreen}/>
-          <Route to='/all_products' exact component= {ShopScreen}/>
+          <Switch>
+            <Route to="/all" exact component={HomeScreen}/>
+          </Switch>
         </Router>
-      </main>
-    </div>
+      </main>*/}
+    </div> 
   );
 }
 
