2d1f4fc85f2ce24c2e9467c5959f0318f4257108
gmshuvo <gm.shuvo.ru.cse@gmail.com>
2021-05-03 12:35:44 +0600

    fixing route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,4 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
-import "./App.css";
-import "./bootstrap.min.css";
-import Header from "./Components/Header/Header";
-import HomeScreen from "./screens/HomeScreen";
-import ShopScreen from "./screens/ShopScreen";
 
-function App() {
-  return (
-    <div className="App">
-     
-      <Header/>
-      <Router>
-      <Switch>
-           
-          <Route to="/all" exact component={HomeScreen}/>
-        <Route path='/collections/products' exact component={ShopScreen}/>\
-          </Switch>
-      </Router>
-
-      {/* <main className="container">
-        <Router>
-          <Switch>
-            <Route to="/all" exact component={HomeScreen}/>
-          </Switch>
-        </Router>
-      </main>*/}
-    </div> 
-  );
-}
-
-export default App;
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
 import "./App.css";
@@ -47,7 +15,7 @@ function App() {
       <main className="container">
         <Router>
           <Switch>
-            <Route to="/all" exact component={HomeScreen}/>
+            <Route path="/" exact component={HomeScreen}/>
              <Route path='/collections/products' exact component={ShopScreen}/>
           </Switch>
         </Router>
